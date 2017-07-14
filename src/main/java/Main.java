7f4286dfc477c04c19d7883e2f09e7948aa2c89d
Igor <alterg@niuitmo.ru>
2017-07-14 07:21:22 +0300

    Change mapping
    * change /authform to /* (simple logic, test stage)
    * refactor (servlet name)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,18 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * Created by alterG on 14.07.2017.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-//        Frontend fronted = new Frontend();
+        AllRequestServlet allRequestServlet = new AllRequestServlet();
         Server server = new Server(8080);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
-//        context.addServlet(new ServletHolder(frontend), "/authform");
+        context.addServlet(new ServletHolder(allRequestServlet), "/*");
         server.start();
         server.join();
     }
