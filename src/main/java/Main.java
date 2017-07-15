8d04055f32acf73b015a9530cce3b5ea3415e33f
Igor <alterg@niuitmo.ru>
2017-07-15 08:24:35 +0300

    add import
    I am wondered about it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestServlet;
 
 /**
  * Created by alterG on 14.07.2017.
