b6a9cef6e2b898acba02311f639a4dbc9c155fc4
Igor <alterg@niuitmo.ru>
2017-07-16 05:56:31 +0300

    Add authorization logic
    * 2 new servlets

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,22 +1,44 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+1
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestServlet;
+import servlets.ServletTask1;
+import servlets.SessionsServlet;
+import servlets.UsersServlet;
 
 /**
  * Created by alterG on 14.07.2017.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestServlet allRequestServlet = new AllRequestServlet();
-        Server server = new Server(8080);
+        AccountService accountService = new AccountService();
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(allRequestServlet), "/*");
+        context.addServlet(new ServletHolder(new UsersServlet()), "api/users");
+        context.addServlet(new ServletHolder(new SessionsServlet()), "api/sessions");
+//        context.addServlet(new ServletHolder(new AllRequestServlet()), "/*");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resourceHandler, context});
+
+        Server server = new Server(8080);
+        server.setHandler(handlers);
+
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
