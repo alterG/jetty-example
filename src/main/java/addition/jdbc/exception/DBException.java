fd26367537dc3d7979ddf9bf8ac013e855c3bdc8
Igor <alterg@niuitmo.ru>
2017-07-18 02:23:28 +0300

    DBException

---
+++ src/main/java/addition/jdbc/exception/DBException.java
@@ -0,0 +1,11 @@
+package addition.jdbc.exception;
+
+/**
+ * Created by alterG on 18.07.2017.
+ */
+public class DBException extends Exception {
+
+    public DBException(Throwable throwable) {
+        super(throwable);
+    }
+}
