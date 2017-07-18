0140786a4e22a69425f81f0a75202b460185aeba
Igor <alterg@niuitmo.ru>
2017-07-18 04:47:25 +0300

    Data base primary usage demonstration code

---
+++ src/main/java/addition/jdbc/Main.java
@@ -0,0 +1,26 @@
+package addition.jdbc;
+
+import addition.jdbc.service.DBService;
+import addition.jdbc.service.dataset.UserDataSet;
+import addition.jdbc.service.exception.DBException;
+
+/**
+ * Created by alterG on 17.07.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DBService dbService = new DBService();
+        dbService.printConnectionInfo();
+
+        try {
+            long userId = dbService.addUser("tully");
+            System.out.println("Add new user id = " + userId);
+
+            UserDataSet userDataSet = dbService.getUser(userId);
+            System.out.println("New user is " + userDataSet);
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
